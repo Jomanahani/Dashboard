da333b9ee66b96a9fc8d777c825cbe1c9e5c5ae9
Jomanahani <jomanahani123456@gmail.com>
2023-03-19 18:26:10 +0200

    add stepper

--- src/Components/Steps/style.js
+++ src/Components/Steps/style.js
@@ -1,49 +1,94 @@
 import styled from "styled-components";
 
 export const UserFlow = styled.aside`
-  border-left:  2px solid ${(props) => props.theme.pallet.Lines};
-`;
-
-export const Step = styled.li`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  border-left: 2px solid ${(props) => props.theme.pallet.Lines};
+  & > div {
+    margin: 2rem auto;
+  }
+`;
+export const StepItem = styled.div`
+  display: flex;
   align-items: center;
-  margin-bottom: 20px;
+  margin-bottom: 0.5rem;
+  & > div:first-of-type {
+    &::after {
+      content: "";
+      position: relative;
+      top: 2rem;
+      left: 3px;
+      height: 1rem;
+      border-left: 3px solid #ccc;
+      z-index: -1;
+    }
+  }
+
+  &:last-of-type {
+    & > div::after {
+      content: "";
+      height: 0rem;
+    }
+  }
 `;
 
 export const Circle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 30px;
-  height: 30px;
+  width: 3rem;
+  height: 3rem;
+  border: 1px solid #f1f1f1;
   border-radius: 50%;
-  background-color: ${(props) => (props.active ? "green" : "white")};
-  border: 2px solid green;
-`;
-
-export const Check = styled.span`
+  background-color: ${({ isActive }) => (isActive ? "#D84E67" : "#F1F1F1")};
+  color: white;
   display: flex;
-  justify-content: center;
   align-items: center;
-  width: 20px;
-  height: 20px;
-  font-size: 16px;
-  color: green;
-  visibility: ${(props) => (props.active ? "visible" : "hidden")};
+  justify-content: center;
+  font-weight: bold;
+  margin-right: 10px;
+  cursor: pointer;
 `;
 
-export const Name = styled.span`
-  margin-left: 10px;
-  font-size: 16px;
-  color: ${(props) => (props.active ? "green" : "gray")};
+export const Label = styled.div`
+  font-size: 1rem;
+  color: ${(props) => props.theme.pallet.primary};
+  font-weight: 500;
+  margin: 0 0.5rem;
 `;
 
-export const Line = styled.div`
-  width: 2px;
-  height: 100%;
-  background-color: green;
-  margin-left: 14px;
-  margin-right: 6px;
-  visibility: ${(props) => (props.last ? "hidden" : "visible")};
+export const SubStepContainer = styled.div`
+  margin-right: 3rem;
+`;
+
+export const SubStep = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+
+  & > div:first-of-type {
+    &::after {
+      content: "";
+      position: relative;
+      top: 1.7rem;
+      left: 3px;
+      height: 1rem;
+      border-left: 3px solid #ccc;
+      z-index: -1;
+    }
+  }
+
+  &:last-of-type {
+    & > div::after {
+      content: "";
+      height: 0rem;
+    }
+  }
+`;
+
+export const SubStepCircle = styled(Circle)`
+  height: 2.5rem;
+  width: 2.5rem;
+`;
+
+export const SubStepLabel = styled(Label)`
+  font-size: 16px;
+  margin-left: 5px;
 `;
